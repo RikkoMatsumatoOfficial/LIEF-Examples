88bd0cf8d87871a0ca2529c1379ca3a995a3bb2a
Rikko Matsumato <kuttimsx@hotmail.com>
2025-05-18 12:10:36 +0300

    Update checkvirt_addr.py

--- checkvirt_addr.py
+++ checkvirt_addr.py
@@ -3,9 +3,9 @@ import os
 def Main():
     lief.disable_leak_warning()
     pe_bin = lief.parse("C:\\Windows\\explorer.exe")
-    for peb in pe_bin.sections:
-        print(peb.virtual_address)
-        print(peb.size)
+    for pe_section_expl in pe_bin.sections:
+        print(pe_section_expl.virtual_address)
+        print(pe_section_expl.size)
         print("This Example is Created by RikkoMatsumatoOfficial!!!")
         os._exit(334)
 
